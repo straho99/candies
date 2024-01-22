ed817c27ddee529b6c3e5d10c3a1bcdede56bafe
StrahilRuychev <strahil.ruychev@gmail.com>
2024-01-22 18:29:09 +0200

    Candies

--- candies/main.cpp
+++ candies/main.cpp
@@ -1,14 +1,31 @@
-//
-//  main.cpp
-//  candies
-//
-//  Created by Strahil Ruychev on 22.01.24.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main() {
+    int bowl1, bowl2, bowl3;
+    int eaten = 0;
+    cin >> bowl1 >> bowl2 >> bowl3;
+    
+    int minCandies = 0;
+    
+    if (minCandies > 5) {
+        minCandies = min(bowl1, bowl2);
+        minCandies = min(minCandies, bowl3) - 1;
+        eaten = minCandies * 3;
+        bowl1 -= minCandies;
+        bowl2 -= minCandies;
+        bowl3 -= minCandies;
+    }
+    
+    
+    while (true)
+    {
+        if (bowl1 > 0) {bowl1--; eaten++;} else {cout << eaten << endl; return 0;}
+        if (bowl2 > 0) {bowl2--; eaten++;} else {cout << eaten << endl; return 0;}
+        if (bowl3 > 0) {bowl3--; eaten++;} else {cout << eaten << endl; return 0;}
+        if (bowl2 > 0) {bowl2--; eaten++;} else {cout << eaten << endl; return 0;}
+    }
+    
     return 0;
 }
