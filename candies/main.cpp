0dc7263085106863c556ef3b509f98de04e9b29d
StrahilRuychev <strahil.ruychev@gmail.com>
2024-01-23 15:58:24 +0200

    Final solution

--- candies/main.cpp
+++ candies/main.cpp
@@ -7,17 +7,15 @@ int main() {
     int eaten = 0;
     cin >> bowl1 >> bowl2 >> bowl3;
     
-    int minCandies = 0;
+    int minCandies = min(bowl1, bowl2/2);
+    minCandies = min(minCandies, bowl3) - 1;
     
     if (minCandies > 5) {
-        minCandies = min(bowl1, bowl2);
-        minCandies = min(minCandies, bowl3) - 1;
-        eaten = minCandies * 3;
         bowl1 -= minCandies;
-        bowl2 -= minCandies;
+        bowl2 -= minCandies * 2;
         bowl3 -= minCandies;
+        eaten = minCandies * 4;
     }
-    
     
     while (true)
     {
